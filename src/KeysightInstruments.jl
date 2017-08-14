99d11de71e0226e5066de3cbc514ec112435439b
Eunjong Kim <ekim7206@gmail.com>
2017-08-14 14:37:41 -0700

    renamed KeysightDigitizer repository into KeysightInstruments in order to include AWG

---
+++ src/KeysightInstruments.jl
@@ -0,0 +1,9 @@
+module KeysightInstruments
+# Based on user's guide for M31XXA/M33XXA Digitizers:
+# http://literature.cdn.keysight.com/litweb/pdf/M3100-90002.pdf?id=2796080
+# and for M32XXA/M33XXA AWGs:
+# http://literature.cdn.keysight.com/litweb/pdf/M3201-90001.pdf?id=2787170
+using Compat
+
+
+end # module
