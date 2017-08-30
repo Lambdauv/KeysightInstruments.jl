05c84c7557198453422c7901c8528036a92f1882
Eunjong Kim <ekim7206@gmail.com>
2017-08-30 11:17:35 -0700

    SD_Wave.jl updated basedon SD_WAVE.h

--- src/SD_Wave.jl
+++ src/SD_Wave.jl
@@ -20,22 +20,31 @@ components (I and Q) --> WAVE_IQ = 2
 doubles, with two components (Module and Phase) --> WAVE_IQPOLAR = 3
 - Digital : Digital waveforms defined with integers --> WAVE_DIGITAL = 5
 """
-SD_Wave_newFromArrayDouble(waveformType::Integer, waveformPoints::Integer,
-    waveformDataA::Array{Float64}, waveformDataB::Array{Float64}=0) =
-    ccall((:SD_Wave_newFromArrayDouble, lib), Cint,
-        (Cint, Cint, Ref{Cdouble}, Ref{Cdouble}),
-        waveformType, waveformPoints, waveformDataA, waveformDataB)
+## int SD_Wave_newFromArrayInteger(int waveformType, int waveformPoints, int *waveformDataA, int *waveformDataB SD_DEFAULT_NULL);
+SD_Wave_newFromArrayInteger(waveformType::Integer, waveformDataA,
+    waveformDataB=0) =
+    ccall((:SD_Wave_newFromArrayInteger, klib), Cint,
+        (Cint, Cint, Ref{Cint}, Ref{Cint}),
+        waveformType, length(waveformDataA), waveformDataA, waveformDataB)
 
+## int SD_Wave_newFromArrayDouble(int waveformType, int waveformPoints, double *waveformDataA, double *waveformDataB SD_DEFAULT_NULL);
+SD_Wave_newFromArrayDouble(waveformType::Integer, waveformDataA,
+    waveformDataB=0) =
+    ccall((:SD_Wave_newFromArrayDouble, klib), Cint,
+        (Cint, Cint, Ref{Cdouble}, Ref{Cdouble}),
+        waveformType, length(waveformDataA), waveformDataA, waveformDataB)
+
+## int SD_Wave_newFromFile(const char *waveformFile);
 """
 This function creates a waveform object from data points contained in an array
 in a file.
 """
 SD_Wave_newFromFile(waveformFile::String) =
-    ccall((:SD_Wave_newFromFile, lib), Cint, (Cstring,), waveformFile)
+    ccall((:SD_Wave_newFromFile, klib), Cint, (Cstring,), waveformFile)
 
-## delete
+## int SD_Wave_delete(int waveformID);
 """
 This function removes a waveform created with the function.
 """
 SD_Wave_delete(waveformID::Integer) =
-    ccall((:SD_Wave_delete, lib), Cint, (Cint,), waveformID)
+    ccall((:SD_Wave_delete, klib), Cint, (Cint,), waveformID)
