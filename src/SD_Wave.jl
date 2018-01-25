dd61fb5cbf13a6c76fca744993155913bf03e3af
Eunjong Kim <ekim7206@gmail.com>
2018-01-25 13:32:32 -0800

    old changes

--- src/SD_Wave.jl
+++ src/SD_Wave.jl
@@ -23,7 +23,7 @@ doubles, with two components (Module and Phase) --> WAVE_IQPOLAR = 3
 """
 ## int SD_Wave_newFromArrayInteger(int waveformType, int waveformPoints, int *waveformDataA, int *waveformDataB SD_DEFAULT_NULL);
 function SD_Wave_newFromArrayInteger(waveformType::Integer,
-    waveformDataA::Vector, waveformDataB::Vector=[]])
+    waveformDataA::Vector, waveformDataB::Vector=[])
     c_waveformDataA = Vector{Cint}(waveformDataA)
     c_waveformDataB = Vector{Cint}(waveformDataB)
     waveformPoints = length(c_waveformDataA)
